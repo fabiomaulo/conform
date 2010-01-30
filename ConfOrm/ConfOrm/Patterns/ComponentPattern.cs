aa98006b8f687f4713abf89b03aff66ef643310d
Fabio Maulo <fabiomaulo@gmail.com>
2010-01-30 20:03:12 +0000

    ComponentPattern

---
+++ ConfOrm/ConfOrm/Patterns/ComponentPattern.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ConfOrm.Patterns
+{
+	public class ComponentPattern : IPattern<Type>
+	{
+		private const BindingFlags DefaultBinding =
+			BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+		private readonly List<IPattern<MemberInfo>> poidPatterns;
+
+		public ComponentPattern()
+		{
+			poidPatterns = new List<IPattern<MemberInfo>> {new PoIdPattern()};
+		}
+
+		#region Implementation of IPattern<Type>
+
+		public bool Match(Type subject)
+		{
+			return
+				subject.GetProperties(DefaultBinding).Cast<MemberInfo>().Concat(subject.GetFields(DefaultBinding)).All(
+					mi => !poidPatterns.Any(p => p.Match(mi)));
+		}
+
+		#endregion
+	}
+}
---
+++ ConfOrm/ConfOrmTests/Patterns/ComponentPatternTest.cs
@@ -0,0 +1,44 @@
+using ConfOrm.Patterns;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.Patterns
+{
+	public class ComponentPatternTest
+	{
+		// a class without Poid is a Component
+		private class AComponent
+		{
+			public string S { get; set; }
+		}
+		private class AEntity
+		{
+			public int Id { get; set; }
+		}
+		private class Entity
+		{
+			private int id;
+		}
+
+		[Test]
+		public void ClassWithoutPoidIsComponent()
+		{
+			var p = new ComponentPattern();
+			p.Match(typeof (AComponent)).Should().Be.True();
+		}
+
+		[Test]
+		public void ClassWithPoidIsNotComponent()
+		{
+			var p = new ComponentPattern();
+			p.Match(typeof(AEntity)).Should().Be.False();
+		}
+
+		[Test]
+		public void ClassWithPoidFieldIsNotComponent()
+		{
+			var p = new ComponentPattern();
+			p.Match(typeof(Entity)).Should().Be.False();
+		}
+	}
+}
