0f6c53231ef1b9dfe600fa67c5a2ad99c98eb583
Fabio Maulo <fabiomaulo@gmail.com>
2010-02-01 17:08:57 +0000

    Refactoring

--- ConfOrm/ConfOrm/Patterns/BagCollectionPattern.cs
+++ ConfOrm/ConfOrm/Patterns/BagCollectionPattern.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 namespace ConfOrm.Patterns
@@ -13,21 +11,8 @@ namespace ConfOrm.Patterns
 		protected override bool MemberMatch(MemberInfo subject)
 		{
 			Type memberType = subject.GetPropertyOrFieldType();
-			if (typeof(IEnumerable).IsAssignableFrom(memberType) && !IsNotSupportedAsBag(memberType))
-			{
-				return true;
-			}
-			if (memberType.IsGenericType)
-			{
-				List<Type> interfaces =
-					memberType.GetInterfaces().Where(t => t.IsGenericType).Select(t => t.GetGenericTypeDefinition()).ToList();
-				if (memberType.IsInterface)
-				{
-					interfaces.Add(memberType.GetGenericTypeDefinition());
-				}
-				return interfaces.Contains(typeof(IEnumerable<>));
-			}
-			return false;
+			// IEnumerable<T> inherits from IEnumerable
+			return typeof(IEnumerable).IsAssignableFrom(memberType) && !IsNotSupportedAsBag(memberType);
 		}
 
 		#endregion
