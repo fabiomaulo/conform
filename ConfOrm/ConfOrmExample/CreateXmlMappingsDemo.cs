ab49d4c28fe450cb356f341c4cd3669e4ee52de3
Fabio Maulo <fabiomaulo@gmail.com>
2010-02-04 23:03:29 +0000

    Removed duplicated code

--- ConfOrm/ConfOrmExample/CreateXmlMappingsDemo.cs
+++ ConfOrm/ConfOrmExample/CreateXmlMappingsDemo.cs
@@ -1,12 +1,7 @@
 using System;
 using System.IO;
-using System.Linq;
 using System.Xml;
 using System.Xml.Serialization;
-using ConfOrm;
-using ConfOrm.NH;
-using ConfOrm.Patterns;
-using ConfOrmExample.Domain;
 using NHibernate.Cfg.MappingSchema;
 using NUnit.Framework;
 
@@ -17,32 +12,8 @@ namespace ConfOrmExample
 		[Test, Explicit]
 		public void ShowSingleXmlMapping()
 		{
-			var document = Serialize(GetMapping());
+			var document = Serialize(NHIntegrationTest.GetMapping());
 			Console.Write(document);
-		}
-
-		public static ObjectRelationalMapper GetMappedDomain()
-		{
-			var orm = new ObjectRelationalMapper();
-
-			orm.TablePerClass<Animal>();
-			orm.TablePerClass<User>();
-			orm.TablePerClass<StateProvince>();
-			orm.TablePerClassHierarchy<Zoo>();
-
-			orm.ManyToMany<Human, Human>();
-			orm.OneToOne<User, Human>();
-
-			orm.PoidStrategies.Add(new NativePoidPattern());
-
-			return orm;
-		}
-
-		public static HbmMapping GetMapping()
-		{
-			var orm = GetMappedDomain();
-			var mapper = new Mapper(orm);
-			return mapper.CompileMappingFor(typeof(Animal).Assembly.GetTypes().Where(t => t.Namespace == typeof(Animal).Namespace));
 		}
 
 		protected static string Serialize(HbmMapping hbmElement)
