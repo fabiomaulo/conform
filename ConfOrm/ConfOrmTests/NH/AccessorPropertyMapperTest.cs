e921150a816f78eace0dba938eed0a6ea9647e57
Fabio Maulo <fabiomaulo@gmail.com>
2010-03-02 04:53:22 +0000

    Refactoring (extracted test)

---
+++ ConfOrm/ConfOrmTests/NH/AccessorPropertyMapperTest.cs
@@ -0,0 +1,137 @@
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.NH
+{
+	public class AccessorPropertyMapperTest
+	{
+		private class MyClass
+		{
+			private string aField;
+			private string withFieldCamelCase;
+			public string WithFieldCamelCase
+			{
+				get { return withFieldCamelCase; }
+				set { withFieldCamelCase = value; }
+			}
+			private string _WithFieldPascalcaseUnderscore;
+			public string WithFieldPascalcaseUnderscore
+			{
+				get { return _WithFieldPascalcaseUnderscore; }
+				set { _WithFieldPascalcaseUnderscore = value; }
+			}
+
+			public string Autoproperty { get; set; }
+			public string ReadOnly { get { return ""; } }
+
+			private string nosetterCamelCase;
+			public string NosetterCamelCase
+			{
+				get { return nosetterCamelCase; }
+			}
+			private string _NosetterPascalcaseUnderscore;
+			public string NosetterPascalcaseUnderscore
+			{
+				get { return _NosetterPascalcaseUnderscore; }
+			}
+
+		}
+
+		[Test]
+		public void WhenNoMemberThenAccessNone()
+		{
+			string accessValue = null;
+
+			new AccessorPropertyMapper(typeof (MyClass), null, x => accessValue = x);
+
+			accessValue.Should().Be.EqualTo("none");
+		}
+
+		[Test]
+		public void WhenNoMemberThenCantChangeAccessor()
+		{
+			string accessValue = null;
+
+			var mapper = new AccessorPropertyMapper(typeof(MyClass), null, x => accessValue = x);
+			mapper.Access(Accessor.Field);
+			accessValue.Should().Be.EqualTo("none");
+		}
+
+		[Test]
+		public void WhenMapFieldAutoAssignAccessField()
+		{
+			string accessValue = null;
+
+			new AccessorPropertyMapper(typeof(MyClass), "aField", x => accessValue = x);
+
+			accessValue.Should().Be.EqualTo("field");
+		}
+
+		[Test]
+		public void WhenMapPropertyNoAutoAssignAccessField()
+		{
+			string accessValue = null;
+
+			new AccessorPropertyMapper(typeof(MyClass), "Autoproperty", x => accessValue = x);
+
+			accessValue.Should().Be.Null();
+		}
+
+		[Test]
+		public void WhenMapPropertyWithFieldPascalcaseUnderscoreThenChooseRigthFieldAccessor()
+		{
+			string accessValue = null;
+			var mapper = new AccessorPropertyMapper(typeof(MyClass), "WithFieldPascalcaseUnderscore", x => accessValue = x);
+
+			mapper.Access(Accessor.Field);
+
+			accessValue.Should().Be.EqualTo("field.pascalcase-underscore");
+		}
+
+		[Test]
+		public void WhenMapPropertyWithFieldCamelCaseThenChooseRigthFieldAccessor()
+		{
+			string accessValue = null;
+			var mapper = new AccessorPropertyMapper(typeof(MyClass), "WithFieldCamelCase", x => accessValue = x);
+
+			mapper.Access(Accessor.NoSetter);
+
+			accessValue.Should().Be.EqualTo("nosetter.camelcase");
+		}
+
+		[Test]
+		public void WhenMapPropertyNosetterPascalcaseUnderscoreThenChooseRigthFieldAccessor()
+		{
+			string accessValue = null;
+			var mapper = new AccessorPropertyMapper(typeof(MyClass), "NosetterPascalcaseUnderscore", x => accessValue = x);
+
+			mapper.Access(Accessor.NoSetter);
+
+			accessValue.Should().Be.EqualTo("nosetter.pascalcase-underscore");
+		}
+
+		[Test]
+		public void WhenMapPropertyNosetterCamelCaseThenChooseRigthFieldAccessor()
+		{
+			string accessValue = null;
+			var mapper = new AccessorPropertyMapper(typeof(MyClass), "NosetterCamelCase", x => accessValue = x);
+
+			mapper.Access(Accessor.NoSetter);
+
+			accessValue.Should().Be.EqualTo("nosetter.camelcase");
+		}
+
+		[Test]
+		public void WhenMapReadOnlyPropertyThenReadonlyAccessor()
+		{
+			string accessValue = null;
+			var mapper = new AccessorPropertyMapper(typeof(MyClass), "ReadOnly", x => accessValue = x);
+
+			mapper.Access(Accessor.ReadOnly);
+
+			accessValue.Should().Be.EqualTo("readonly");
+		}
+	}
+}
--- ConfOrm/ConfOrmTests/NH/PropertyMapperTest.cs
+++ ConfOrm/ConfOrmTests/NH/PropertyMapperTest.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Linq;
 using System.Data;
-using System.Reflection;
-using ConfOrm.Mappers;
 using ConfOrm.NH;
 using NHibernate;
 using NHibernate.Cfg.MappingSchema;
@@ -18,136 +16,8 @@ namespace ConfOrmTests.NH
 	{
 		private class MyClass
 		{
-			private string aField;
-			private string withFieldCamelCase;
-			public string WithFieldCamelCase
-			{
-				get { return withFieldCamelCase; }
-				set { withFieldCamelCase = value; }
-			}
-			private string _WithFieldPascalcaseUnderscore;
-			public string WithFieldPascalcaseUnderscore
-			{
-				get { return _WithFieldPascalcaseUnderscore; }
-				set { _WithFieldPascalcaseUnderscore = value; }
-			}
-
 			public string Autoproperty { get; set; }
 			public string ReadOnly { get { return "";}}
-
-			private string nosetterCamelCase;
-			public string NosetterCamelCase
-			{
-				get { return nosetterCamelCase; }
-			}
-			private string _NosetterPascalcaseUnderscore;
-			public string NosetterPascalcaseUnderscore
-			{
-				get { return _NosetterPascalcaseUnderscore; }
-			}
-
-		}
-
-		[Test]
-		public void WhenNoMemberThenAccessNone()
-		{
-			var mapping = new HbmProperty();
-
-			new PropertyMapper(null, mapping);
-
-			mapping.Access.Should().Be.EqualTo("none");
-		}
-
-		[Test]
-		public void WhenNoMemberThenCantChangeAccessor()
-		{
-			var mapping = new HbmProperty();
-
-			var mapper = new PropertyMapper(null, mapping);
-			mapper.Access(Accessor.Field);
-			mapping.Access.Should().Be.EqualTo("none");
-		}
-
-		[Test]
-		public void WhenMapFieldAutoAssignAccessField()
-		{
-			var member = typeof (MyClass).GetField("aField", BindingFlags.Instance | BindingFlags.NonPublic);
-			var mapping = new HbmProperty();
-			
-			new PropertyMapper(member, mapping);
-
-			mapping.Access.Should().Be.EqualTo("field");
-		}
-
-		[Test]
-		public void WhenMapPropertyNoAutoAssignAccessField()
-		{
-			var member = typeof(MyClass).GetProperty("Autoproperty");
-			var mapping = new HbmProperty();
-
-			new PropertyMapper(member, mapping);
-
-			mapping.Access.Should().Be.Null();
-		}
-
-		[Test]
-		public void WhenMapPropertyWithFieldPascalcaseUnderscoreThenChooseRigthFieldAccessor()
-		{
-			var member = typeof(MyClass).GetProperty("WithFieldPascalcaseUnderscore");
-			var mapping = new HbmProperty();
-			var mapper = new PropertyMapper(member, mapping);
-			
-			mapper.Access(Accessor.Field);
-
-			mapping.Access.Should().Be.EqualTo("field.pascalcase-underscore");
-		}
-
-		[Test]
-		public void WhenMapPropertyWithFieldCamelCaseThenChooseRigthFieldAccessor()
-		{
-			var member = typeof(MyClass).GetProperty("WithFieldCamelCase");
-			var mapping = new HbmProperty();
-			var mapper = new PropertyMapper(member, mapping);
-
-			mapper.Access(Accessor.NoSetter);
-
-			mapping.Access.Should().Be.EqualTo("nosetter.camelcase");
-		}
-
-		[Test]
-		public void WhenMapPropertyNosetterPascalcaseUnderscoreThenChooseRigthFieldAccessor()
-		{
-			var member = typeof(MyClass).GetProperty("NosetterPascalcaseUnderscore");
-			var mapping = new HbmProperty();
-			var mapper = new PropertyMapper(member, mapping);
-
-			mapper.Access(Accessor.NoSetter);
-
-			mapping.Access.Should().Be.EqualTo("nosetter.pascalcase-underscore");
-		}
-
-		[Test]
-		public void WhenMapPropertyNosetterCamelCaseThenChooseRigthFieldAccessor()
-		{
-			var member = typeof(MyClass).GetProperty("NosetterCamelCase");
-			var mapping = new HbmProperty();
-			var mapper = new PropertyMapper(member, mapping);
-
-			mapper.Access(Accessor.NoSetter);
-
-			mapping.Access.Should().Be.EqualTo("nosetter.camelcase");
-		}
-
-		[Test]
-		public void WhenMapReadOnlyPropertyThenReadonlyAccessor()
-		{
-			var member = typeof(MyClass).GetProperty("ReadOnly");
-			var mapping = new HbmProperty();
-			var mapper = new PropertyMapper(member, mapping);
-
-			mapper.Access(Accessor.ReadOnly);
-
-			mapping.Access.Should().Be.EqualTo("readonly");
 		}
 
 		[Test]
