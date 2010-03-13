10968c4cc3a3ce5eeecae8a4ea5277b846340cf8
Fabio Maulo <fabiomaulo@gmail.com>
2010-03-13 16:58:18 +0000

    Test to pass for unidirectional one-To-one

---
+++ ConfOrm/ConfOrmTests/ObjectRelationalMapperTests/UnidirectionalOneToOneTest.cs
@@ -0,0 +1,31 @@
+using ConfOrm;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.ObjectRelationalMapperTests
+{
+	public class UnidirectionalOneToOneTest
+	{
+		// a unidirectional One-To-One should be mapped as a many-to-one
+		private class MyClass
+		{
+			public int Id { get; set; }
+			public MyOneClass MyOneClass { get; set; }
+		}
+		private class MyOneClass
+		{
+			public int Id { get; set; }
+		}
+
+		[Test, Ignore("Not fixed yet.")]
+		public void WhenUnidirectionalOneToOneThenShouldBeManyToOne()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass(new[] {typeof (MyClass), typeof (MyOneClass)});
+			orm.OneToOne<MyClass, MyOneClass>();
+
+			orm.IsOneToOne(typeof (MyClass), typeof (MyOneClass)).Should().Be.False();
+			orm.IsManyToOne(typeof(MyClass), typeof(MyOneClass)).Should().Be.True();
+		}
+	}
+}
