3a12fbbe5f57c4caf5e4a7729366f8740874a34b
Fabio Maulo <fabiomaulo@gmail.com>
2010-03-30 17:22:46 +0000

    JoinedSubclassKeyAsRootIdColumnApplier

---
+++ ConfOrm/ConfOrm.Shop/Appliers/JoinedSubclassKeyAsRootIdColumnApplier.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using ConfOrm.Mappers;
+
+namespace ConfOrm.Shop.Appliers
+{
+	public class JoinedSubclassKeyAsRootIdColumnApplier: IPatternApplier<Type, IJoinedSubclassAttributesMapper>
+	{
+		private readonly IDomainInspector domainInspector;
+
+		public JoinedSubclassKeyAsRootIdColumnApplier(IDomainInspector domainInspector)
+		{
+			if (domainInspector == null)
+			{
+				throw new ArgumentNullException("domainInspector");
+			}
+			this.domainInspector = domainInspector;
+		}
+
+		#region Implementation of IPattern<Type>
+
+		public virtual bool Match(Type subject)
+		{
+			return true;
+		}
+
+		#endregion
+
+		#region Implementation of IPatternApplier<Type,IJoinedSubclassAttributesMapper>
+
+		public void Apply(Type subject, IJoinedSubclassAttributesMapper applyTo)
+		{
+			var idMember = GetPoidPropertyOrField(subject);
+			if(idMember != null)
+			{
+				applyTo.Key(km => km.Column(GetColumnNameForPoid(idMember)));
+			}
+		}
+
+		#endregion
+
+		protected MemberInfo GetPoidPropertyOrField(Type type)
+		{
+			return type.GetProperties().Cast<MemberInfo>().Concat(type.GetFields()).FirstOrDefault(mi => domainInspector.IsPersistentId(mi));
+		}
+
+		protected virtual string GetColumnNameForPoid(MemberInfo poidMember)
+		{
+			return poidMember.Name;
+		}
+	}
+}
---
+++ ConfOrm/ConfOrm.ShopTests/AppliersTests/JoinedSubclassKeyAsRootIdColumnApplierTest.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Reflection;
+using ConfOrm.Mappers;
+using ConfOrm.Shop.Appliers;
+using Moq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrm.ShopTests.AppliersTests
+{
+	public class JoinedSubclassKeyAsRootIdColumnApplierTest
+	{
+		private class MyClass
+		{
+			public int Id { get; set; }
+			public int PoId { get; set; }
+		}
+
+		private class Inherited : MyClass
+		{
+		}
+
+		[Test]
+		public void AlwaysMatch()
+		{
+			var inspector = new Mock<IDomainInspector>();
+			var pattern = new JoinedSubclassKeyAsRootIdColumnApplier(inspector.Object);
+			pattern.Match(null).Should().Be.True();
+		}
+
+		[Test]
+		public void WhenBaseIsIdThenUseId()
+		{
+			var inspector = new Mock<IDomainInspector>();
+			inspector.Setup(x => x.IsPersistentId(It.Is<MemberInfo>(mi => mi.Name == "Id"))).Returns(true);
+			var pattern = new JoinedSubclassKeyAsRootIdColumnApplier(inspector.Object);
+			var mapper = new Mock<IJoinedSubclassAttributesMapper>();
+			var keyMapper = new Mock<IKeyMapper>();
+			mapper.Setup(x => x.Key(It.IsAny<Action<IKeyMapper>>())).Callback<Action<IKeyMapper>>(
+				x => x.Invoke(keyMapper.Object));
+
+			pattern.Apply(typeof (Inherited), mapper.Object);
+
+			keyMapper.Verify(km => km.Column(It.Is<string>(s => s == "Id")));
+		}
+
+		[Test]
+		public void WhenBaseIsPoIdThenUsePoId()
+		{
+			var inspector = new Mock<IDomainInspector>();
+			inspector.Setup(x => x.IsPersistentId(It.Is<MemberInfo>(mi => mi.Name == "PoId"))).Returns(true);
+			var pattern = new JoinedSubclassKeyAsRootIdColumnApplier(inspector.Object);
+			var mapper = new Mock<IJoinedSubclassAttributesMapper>();
+			var keyMapper = new Mock<IKeyMapper>();
+			mapper.Setup(x => x.Key(It.IsAny<Action<IKeyMapper>>())).Callback<Action<IKeyMapper>>(
+				x => x.Invoke(keyMapper.Object));
+
+			pattern.Apply(typeof(Inherited), mapper.Object);
+
+			keyMapper.Verify(km => km.Column(It.Is<string>(s => s == "PoId")));
+		}
+
+		[Test]
+		public void WhenNoPoidMemberThenDoesNotSet()
+		{
+			var inspector = new Mock<IDomainInspector>();
+			var pattern = new JoinedSubclassKeyAsRootIdColumnApplier(inspector.Object);
+			var mapper = new Mock<IJoinedSubclassAttributesMapper>();
+			var keyMapper = new Mock<IKeyMapper>();
+			mapper.Setup(x => x.Key(It.IsAny<Action<IKeyMapper>>())).Callback<Action<IKeyMapper>>(
+				x => x.Invoke(keyMapper.Object));
+
+			pattern.Apply(typeof(Inherited), mapper.Object);
+
+			keyMapper.Verify(km => km.Column(It.IsAny<string>()), Times.Never());
+		}
+	}
+}
