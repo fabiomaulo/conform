e1a56eed26fcae7f0648488bca491b4e5df48727
Fabio Maulo <fabiomaulo@gmail.com>
2010-04-03 18:28:01 +0000

    ManyToOne column applier

---
+++ ConfOrm/ConfOrm.Shop/Appliers/ManyToOneColumnApplier.cs
@@ -0,0 +1,32 @@
+using System;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+
+namespace ConfOrm.Shop.Appliers
+{
+	public class ManyToOneColumnApplier: IPatternApplier<PropertyPath, IManyToOneMapper>
+	{
+		#region Implementation of IPattern<PropertyPath>
+
+		public bool Match(PropertyPath subject)
+		{
+			return true;
+		}
+
+		#endregion
+
+		#region Implementation of IPatternApplier<PropertyPath,IManyToOneMapper>
+
+		public void Apply(PropertyPath subject, IManyToOneMapper applyTo)
+		{
+			applyTo.Column(GetRelationColumnName(subject));
+		}
+
+		#endregion
+
+		protected virtual string GetRelationColumnName(PropertyPath subject)
+		{
+			return subject.ToColumnName() + "Id";
+		}
+	}
+}
---
+++ ConfOrm/ConfOrm.ShopTests/AppliersTests/ManyToOneColumnApplierTest.cs
@@ -0,0 +1,55 @@
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Shop.Appliers;
+using Moq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrm.ShopTests.AppliersTests
+{
+	public class ManyToOneColumnApplierTest
+	{
+		private class MyClass
+		{
+			public MyOtherClass OtherClass { get; set; }
+			public MyComponent Component { get; set; }
+		}
+
+		private class MyOtherClass
+		{
+
+		}
+
+		private class MyComponent
+		{
+			public MyOtherClass AnotherClass { get; set; }			
+		}
+
+		[Test]
+		public void WhenInEntityThenApplyPropertyName()
+		{
+			var applier = new ManyToOneColumnApplier();
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(x => x.OtherClass));
+			var mayToOneMapper = new Mock<IManyToOneMapper>();
+
+			applier.Match(path).Should().Be.True();
+			applier.Apply(path, mayToOneMapper.Object);
+
+			mayToOneMapper.Verify(x => x.Column(It.Is<string>(columnName => columnName == "OtherClassId")));
+		}
+
+		[Test]
+		public void WhenInComponentThenApplyPropertyPath()
+		{
+			var applier = new ManyToOneColumnApplier();
+			var level0 = new PropertyPath(null, ForClass<MyClass>.Property(x => x.Component));
+			var path = new PropertyPath(level0, ForClass<MyComponent>.Property(x => x.AnotherClass));
+			var mayToOneMapper = new Mock<IManyToOneMapper>();
+
+			applier.Match(path).Should().Be.True();
+			applier.Apply(path, mayToOneMapper.Object);
+
+			mayToOneMapper.Verify(x => x.Column(It.Is<string>(columnName => columnName == "ComponentAnotherClassId")));
+		}
+	}
+}
