09364a3c04ea97c88180679568df0aaba5130055
Fabio Maulo <fabiomaulo@gmail.com>
2010-04-04 04:32:01 +0000

    Alternative default PatternsAppliersHolder with columns/table naming

---
+++ ConfOrm/ConfOrm.Shop/CoolNaming/CoolPatternsAppliersHolder.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Patterns;
+
+namespace ConfOrm.Shop.CoolNaming
+{
+	public class CoolPatternsAppliersHolder : EmptyPatternsAppliersHolder
+	{
+		public CoolPatternsAppliersHolder(IDomainInspector domainInspector)
+		{
+			if (domainInspector == null)
+			{
+				throw new ArgumentNullException("domainInspector");
+			}
+			rootClass = new List<IPatternApplier<Type, IClassAttributesMapper>>();
+			joinedSubclass = new List<IPatternApplier<Type, IJoinedSubclassAttributesMapper>>
+			                 	{new JoinedSubclassOnDeleteApplier(), new JoinedSubclassKeyAsRootIdColumnApplier(domainInspector)};
+			subclass = new List<IPatternApplier<Type, ISubclassAttributesMapper>>();
+			unionSubclass = new List<IPatternApplier<Type, IUnionSubclassAttributesMapper>>();
+			poid = new List<IPatternApplier<MemberInfo, IIdMapper>>
+			       	{
+			       		new MemberNoSetterToFieldAccessorApplier<IIdMapper>(),
+			       		new NoPoidGuidApplier(),
+			       		new BidirectionalOneToOneAssociationPoidApplier(domainInspector)
+			       	};
+			property = new List<IPatternApplier<MemberInfo, IPropertyMapper>>
+			           	{
+			           		new MemberReadOnlyAccessorApplier<IPropertyMapper>(),
+			           		new MemberNoSetterToFieldAccessorApplier<IPropertyMapper>(),
+			           		new MemberToFieldAccessorApplier<IPropertyMapper>(),
+			           	};
+			propertyPath = new List<IPatternApplier<PropertyPath, IPropertyMapper>> {new ComponetPropertyColumnNameApplier(),};
+			collection = new List<IPatternApplier<MemberInfo, ICollectionPropertiesMapper>>
+			             	{
+			             		new MemberReadOnlyAccessorApplier<ICollectionPropertiesMapper>(),
+			             		new MemberNoSetterToFieldAccessorApplier<ICollectionPropertiesMapper>(),
+			             		new MemberToFieldAccessorApplier<ICollectionPropertiesMapper>(),
+			             		new BidirectionalOneToManyApplier(domainInspector),
+			             		new BidirectionalOneToManyOnDeleteConstraintApplier(domainInspector),
+			             		new BidirectionalManyToManyInverseApplier(domainInspector),
+			             	};
+
+			collectionPath = new List<IPatternApplier<PropertyPath, ICollectionPropertiesMapper>>
+			                 	{
+			                 		new ManyToManyInCollectionTableApplier(domainInspector),
+			                 		new ManyToManyKeyIdColumnApplier(domainInspector),
+			                 		new OneToManyKeyColumnApplier(domainInspector),
+			                 	};
+			manyToOne = new List<IPatternApplier<MemberInfo, IManyToOneMapper>>
+			            	{
+			            		new MemberReadOnlyAccessorApplier<IManyToOneMapper>(),
+			            		new MemberNoSetterToFieldAccessorApplier<IManyToOneMapper>(),
+			            		new MemberToFieldAccessorApplier<IManyToOneMapper>(),
+			            		new BidirectionalForeignKeyAssociationManyToOneApplier(domainInspector),
+			            		new UnidirectionalOneToOneUniqueCascadeApplier(domainInspector)
+			            	};
+			manyToOnePath = new List<IPatternApplier<PropertyPath, IManyToOneMapper>> {new ManyToOneColumnApplier()};
+			oneToOne = new List<IPatternApplier<MemberInfo, IOneToOneMapper>>
+			           	{
+			           		new MemberReadOnlyAccessorApplier<IOneToOneMapper>(),
+			           		new MemberNoSetterToFieldAccessorApplier<IOneToOneMapper>(),
+			           		new MemberToFieldAccessorApplier<IOneToOneMapper>(),
+			           		new BidirectionalForeignKeyAssociationOneToOneApplier(domainInspector),
+			           		new BidirectionalPrimaryKeyAssociationMasterOneToOneApplier(domainInspector),
+			           		new BidirectionalPrimaryKeyAssociationSlaveOneToOneApplier(domainInspector)
+			           	};
+			oneToOnePath = new List<IPatternApplier<PropertyPath, IOneToOneMapper>>();
+			any = new List<IPatternApplier<MemberInfo, IAnyMapper>>
+			      	{
+			      		new MemberReadOnlyAccessorApplier<IAnyMapper>(),
+			      		new MemberNoSetterToFieldAccessorApplier<IAnyMapper>(),
+			      		new MemberToFieldAccessorApplier<IAnyMapper>()
+			      	};
+			anyPath = new List<IPatternApplier<PropertyPath, IAnyMapper>>();
+			manyToMany = new List<IPatternApplier<MemberInfo, IManyToManyMapper>>();
+			manyToManyPath = new List<IPatternApplier<PropertyPath, IManyToManyMapper>>
+			                 	{new ManyToManyColumnApplier(domainInspector),};
+		}
+	}
+}
