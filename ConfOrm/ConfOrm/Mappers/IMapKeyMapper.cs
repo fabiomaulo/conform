7f7d76275085762fb346797bf7a6334ad79559d7
Fabio Maulo <fabiomaulo@gmail.com>
2010-04-20 16:31:54 +0000

    Starting re-thinking to map dictionaries

---
+++ ConfOrm/ConfOrm/Mappers/IMapKeyMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NHibernate.Type;
+using NHibernate.UserTypes;
+
+namespace ConfOrm.Mappers
+{
+	public interface IMapKeyMapper : IColumnsMapper
+	{
+		void Type(IType persistentType);
+		void Type<TPersistentType>() where TPersistentType : IUserType;
+		void Length(int length);
+	}
+}
