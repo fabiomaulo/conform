a3b59c1e8cc1d916eb87b4362b33bbd4ede16e0e
Fabio Maulo <fabiomaulo@gmail.com>
2010-04-27 03:34:30 +0000

    MsBuild files

---
+++ ConfOrm/build.bat
@@ -0,0 +1,14 @@
+@echo off
+SET sn="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\sn.exe"
+set gacutil="%programfiles%\Microsoft SDKs\Windows\V6.0A\Bin\gacutil.exe"
+set msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+REM set lib="\lib"
+REM set configuration=Debug
+REM if exist commonfiles\ConfORM.snk goto build
+REM echo Generating strong key...
+REM %sn% -k commonfiles\ConfORM.snk
+:build
+%msbuild% default.msbuild /v:n /t:Build
+:end
+echo -------------------------------
+pause
