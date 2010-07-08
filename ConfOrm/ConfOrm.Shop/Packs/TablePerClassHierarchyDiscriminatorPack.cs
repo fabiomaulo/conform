1e5d25ae9aefb25e520356c2e05cc2dcb0f26ec2
Fabio Maulo <fabiomaulo@gmail.com>
2010-07-08 18:31:05 -0300

    TablePerClassHierarchyDiscriminator Pack

---
+++ ConfOrm/ConfOrm.Shop/Packs/TablePerClassHierarchyDiscriminatorPack.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Shop.Subclassing;
+
+namespace ConfOrm.Shop.Packs
+{
+	public class TablePerClassHierarchyDiscriminatorPack : EmptyPatternsAppliersHolder
+	{
+		public TablePerClassHierarchyDiscriminatorPack(IDomainInspector domainInspector)
+		{
+			rootClass = new List<IPatternApplier<Type, IClassAttributesMapper>>
+			            	{
+			            		new DiscriminatorColumnNameApplier(domainInspector),
+											new ClassDiscriminatorValueAsClassNameApplier(domainInspector)
+			            	};
+			subclass = new List<IPatternApplier<Type, ISubclassAttributesMapper>>
+			           	{
+			           		new SubclassDiscriminatorValueAsClassNameApplier()
+			           	};
+		}
+	}
+}
