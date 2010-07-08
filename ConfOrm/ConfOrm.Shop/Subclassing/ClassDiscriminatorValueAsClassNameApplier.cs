8aeda23a14171fc4973327bb05fd746ff71d4d44
Fabio Maulo <fabiomaulo@gmail.com>
2010-07-08 18:13:35 -0300

    ClassDiscriminatorValueAsClassNameApplier

---
+++ ConfOrm/ConfOrm.Shop/Subclassing/ClassDiscriminatorValueAsClassNameApplier.cs
@@ -0,0 +1,36 @@
+using System;
+using ConfOrm.Mappers;
+
+namespace ConfOrm.Shop.Subclassing
+{
+	public class ClassDiscriminatorValueAsClassNameApplier : IPatternApplier<Type, IClassAttributesMapper>
+	{
+		private readonly IDomainInspector domainInspector;
+
+		public ClassDiscriminatorValueAsClassNameApplier(IDomainInspector domainInspector)
+		{
+			this.domainInspector = domainInspector;
+		}
+
+		#region IPatternApplier<Type,IClassMapper> Members
+
+		public bool Match(Type subject)
+		{
+			return domainInspector.IsTablePerClassHierarchy(subject);
+		}
+
+		public virtual void Apply(Type subject, IClassAttributesMapper applyTo)
+		{
+			applyTo.DiscriminatorValue(subject.Name);
+
+			SetColumnLength(applyTo);
+		}
+
+		protected virtual void SetColumnLength(IClassAttributesMapper applyTo)
+		{
+			applyTo.Discriminator(dm=> dm.Length(64));
+		}
+
+		#endregion
+	}
+}
