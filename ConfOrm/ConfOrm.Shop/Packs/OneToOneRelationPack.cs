e2eb357db72b25626540aba00106a7cf1712b3e7
Fabio Maulo <fabiomaulo@gmail.com>
2010-07-08 18:47:06 -0300

    One-to-One relation pack

---
+++ ConfOrm/ConfOrm.Shop/Packs/OneToOneRelationPack.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Reflection;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Patterns;
+
+namespace ConfOrm.Shop.Packs
+{
+	public class OneToOneRelationPack : EmptyPatternsAppliersHolder
+	{
+		public OneToOneRelationPack(IDomainInspector domainInspector)
+		{
+			poid = new List<IPatternApplier<MemberInfo, IIdMapper>>
+			       	{
+			       		new BidirectionalOneToOneAssociationPoidApplier(domainInspector)
+			       	};
+			manyToOne = new List<IPatternApplier<MemberInfo, IManyToOneMapper>>
+			            	{
+			            		new UnidirectionalOneToOneUniqueCascadeApplier(domainInspector)
+			            	};
+			oneToOne = new List<IPatternApplier<MemberInfo, IOneToOneMapper>>
+			           	{
+			           		new BidirectionalForeignKeyAssociationOneToOneApplier(domainInspector),
+			           		new BidirectionalPrimaryKeyAssociationMasterOneToOneApplier(domainInspector),
+			           		new BidirectionalPrimaryKeyAssociationSlaveOneToOneApplier(domainInspector)
+			           	};
+		}
+	}
+}
