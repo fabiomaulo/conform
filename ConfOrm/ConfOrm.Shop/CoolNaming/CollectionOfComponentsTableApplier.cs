3d05b6352d5a0434f68945d1b9af9ffa01d253ad
Fabio Maulo <fabiomaulo@gmail.com>
2010-07-11 13:54:42 -0300

    CollectionOfComponentsTableApplier

---
+++ ConfOrm/ConfOrm.Shop/CoolNaming/CollectionOfComponentsTableApplier.cs
@@ -0,0 +1,35 @@
+using System;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Shop.Appliers;
+
+namespace ConfOrm.Shop.CoolNaming
+{
+	public class CollectionOfComponentsTableApplier : CollectionOfComponentsPattern,
+	                                                  IPatternApplier<PropertyPath, ICollectionPropertiesMapper>
+	{
+		public CollectionOfComponentsTableApplier(IDomainInspector domainInspector) : base(domainInspector)
+		{
+		}
+
+		#region IPatternApplier<PropertyPath,ICollectionPropertiesMapper> Members
+
+		public bool Match(PropertyPath subject)
+		{
+			return Match(subject.LocalMember);
+		}
+
+		public void Apply(PropertyPath subject, ICollectionPropertiesMapper applyTo)
+		{
+			applyTo.Table(GetTableName(subject));
+		}
+
+		#endregion
+
+		protected virtual string GetTableName(PropertyPath subject)
+		{
+			Type entity = subject.GetContainerEntity(DomainInspector);
+			return entity.Name + subject.ToColumnName();
+		}
+	}
+}
---
+++ ConfOrm/ConfOrm.ShopTests/AppliersTests/CollectionOfComponentsTableApplierTest.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Shop.CoolNaming;
+using Moq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrm.ShopTests.AppliersTests
+{
+	public class CollectionOfComponentsTableApplierTest
+	{
+		private class MyClass
+		{
+			public int Id { get; set; }
+			public MyComponent Component { get; set; }
+			public IList Something { get; set; }
+			public ICollection<MyRelated> Relateds { get; set; }
+			public ICollection<MyComponent> Components { get; set; }
+			public ICollection<string> Elements { get; set; }
+		}
+
+		private class MyComponent
+		{
+			public ICollection<MyRelated> Relateds { get; set; }
+			public ICollection<MyComponent> Components { get; set; }
+			public ICollection<string> Elements { get; set; }
+		}
+
+		private class MyRelated
+		{
+			public int Id { get; set; }
+		}
+
+		[Test]
+		public void WhenNoGenericCollectionThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Something));
+
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenRelationIsOneToManyThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsOneToMany(It.Is<Type>(t => t == typeof(MyClass)), It.Is<Type>(t => t == typeof(MyRelated)))).Returns(true);
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Relateds));
+
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenRelationIsManyToManyThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsManyToMany(It.Is<Type>(t => t == typeof(MyClass)), It.Is<Type>(t => t == typeof(MyRelated)))).Returns(true);
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Relateds));
+
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenRelationIsOneToManyInsideComponentThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsOneToMany(It.Is<Type>(t => t == typeof(MyComponent)), It.Is<Type>(t => t == typeof(MyRelated)))).Returns(true);
+			var path = new PropertyPath(new PropertyPath(null, ForClass<MyClass>.Property(p => p.Components)), ForClass<MyComponent>.Property(p => p.Relateds));
+
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenRelationIsElementsThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Elements));
+
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenRelationIsElementsInsideComponentThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			var path = new PropertyPath(new PropertyPath(null, ForClass<MyClass>.Property(p => p.Components)), ForClass<MyComponent>.Property(p => p.Elements));
+
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenComponentPropertyThenNoMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsComponent(typeof(MyComponent))).Returns(true);
+
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Component));
+			pattern.Match(path).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenComponentsCollectionThenMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsComponent(typeof(MyComponent))).Returns(true);
+
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Components));
+			pattern.Match(path).Should().Be.True();
+		}
+
+		[Test]
+		public void WhenComponentsCollectionInsideComponentThenMatch()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsComponent(typeof(MyComponent))).Returns(true);
+			var path = new PropertyPath(new PropertyPath(null, ForClass<MyClass>.Property(p => p.Component)), ForClass<MyComponent>.Property(p => p.Components));
+
+			pattern.Match(path).Should().Be.True();
+		}
+
+		[Test]
+		public void WhenCollectionIsInPlainEntityThenApplyClassNamePropertyName()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsComponent(typeof(MyComponent))).Returns(true);
+
+			var mapper = new Mock<ICollectionPropertiesMapper>();
+			var path = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Components));
+
+			pattern.Match(path).Should().Be.True();
+			pattern.Apply(path, mapper.Object);
+			mapper.Verify(km => km.Table(It.Is<string>(s => s == "MyClassComponents")));
+		}
+
+		[Test]
+		public void WhenCollectionIsInsideComponentThenApplyClassNameComponentPropertyNamePropertyName()
+		{
+			var orm = new Mock<IDomainInspector>();
+			var pattern = new CollectionOfComponentsTableApplier(orm.Object);
+			orm.Setup(x => x.IsComponent(typeof(MyComponent))).Returns(true);
+
+			var mapper = new Mock<ICollectionPropertiesMapper>();
+			var level0 = new PropertyPath(null, ForClass<MyClass>.Property(p => p.Component));
+			var path = new PropertyPath(level0, ForClass<MyComponent>.Property(p => p.Components));
+
+			pattern.Match(path).Should().Be.True();
+			pattern.Apply(path, mapper.Object);
+			mapper.Verify(km => km.Table(It.Is<string>(s => s == "MyClassComponentComponents")));
+		}
+
+	}
+}
