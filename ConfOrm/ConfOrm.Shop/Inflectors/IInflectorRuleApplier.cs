a36f9fe115388c0807cfce6b34aea774dc7bfcd1
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-08 10:09:17 -0300

    Added commet to the method

--- ConfOrm/ConfOrm.Shop/Inflectors/IInflectorRuleApplier.cs
+++ ConfOrm/ConfOrm.Shop/Inflectors/IInflectorRuleApplier.cs
@@ -2,6 +2,11 @@ namespace ConfOrm.Shop.Inflectors
 {
 	public interface IInflectorRuleApplier
 	{
+		/// <summary>
+		/// Apply the rule to a word.
+		/// </summary>
+		/// <param name="word">The source word.</param>
+		/// <returns>The result of the rule or null where the rule does not apply. </returns>
 		string Apply(string word);
 	}
 }
