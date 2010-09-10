7e4f4173d0002cea49112e7dd5435f6e0875ccc3
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-10 13:28:41 -0300

    Usage example of inflector-based packs

--- ConfOrm/ConfOrm.UsageExamples/Packs/CompositionDemo.cs
+++ ConfOrm/ConfOrm.UsageExamples/Packs/CompositionDemo.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using ConfOrm.NH;
 using ConfOrm.Shop.Appliers;
 using ConfOrm.Shop.CoolNaming;
+using ConfOrm.Shop.InflectorNaming;
+using ConfOrm.Shop.Inflectors;
 using ConfOrm.Shop.Packs;
 using NUnit.Framework;
 
@@ -41,6 +43,38 @@ namespace ConfOrm.UsageExamples.Packs
 
 			// Note: I'm declaring the strategy only for the base entity
 			orm.TablePerClassHierarchy<Animal>();
+
+			// Note : I have to create mappings for the whole domain
+			var mapping = mapper.CompileMappingFor(typeof(Animal).Assembly.GetTypes().Where(t => t.Namespace == typeof(Animal).Namespace));
+			Console.Write(mapping.AsString());
+		}
+
+		[Test, Explicit]
+		public void ComposingPatternsAppliersPacksUsingInflector()
+		{
+			// In this example I'll use the same domain, but composing others packs, changing the strategy for the hierarchy
+
+			var orm = new ObjectRelationalMapper();
+
+			// The follow line show how compose patterns-appliers-packs and patterns-appliers
+			IPatternsAppliersHolder patternsAppliers =
+				(new SafePropertyAccessorPack())
+					.Merge(new OneToOneRelationPack(orm))
+					.Merge(new BidirectionalManyToManyRelationPack(orm))
+					.Merge(new BidirectionalOneToManyRelationPack(orm))
+					.Merge(new DiscriminatorValueAsClassNamePack(orm))
+					.Merge(new CoolColumnsNamingPack(orm))
+					.Merge(new TablePerClassPack())
+					.Merge(new PluralizedTablesPack(orm, new EnglishInflector()))
+					.Merge(new ListIndexAsPropertyPosColumnNameApplier())
+					.Merge(new DatePropertyByNameApplier())
+					.Merge(new MsSQL2008DateTimeApplier());
+
+			// Instancing the Mapper using the result of Merge
+			var mapper = new Mapper(orm, patternsAppliers);
+
+			// Note: I'm declaring the strategy only for the base entity
+			orm.TablePerClass<Animal>();
 
 			// Note : I have to create mappings for the whole domain
 			var mapping = mapper.CompileMappingFor(typeof(Animal).Assembly.GetTypes().Where(t => t.Namespace == typeof(Animal).Namespace));
