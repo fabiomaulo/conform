d84a548158814dc845480d5d7645be88164f460e
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-12 17:17:13 -0300

    Dear DBA ManyToOneColumnApplier

---
+++ ConfOrm/ConfOrm.Shop/DearDbaNaming/ManyToOneColumnApplier.cs
@@ -0,0 +1,31 @@
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+
+namespace ConfOrm.Shop.DearDbaNaming
+{
+	public class ManyToOneColumnApplier: IPatternApplier<PropertyPath, IManyToOneMapper>
+	{
+		#region Implementation of IPattern<PropertyPath>
+
+		public bool Match(PropertyPath subject)
+		{
+			return true;
+		}
+
+		#endregion
+
+		#region Implementation of IPatternApplier<PropertyPath,IManyToOneMapper>
+
+		public void Apply(PropertyPath subject, IManyToOneMapper applyTo)
+		{
+			applyTo.Column(GetRelationColumnName(subject));
+		}
+
+		#endregion
+
+		protected virtual string GetRelationColumnName(PropertyPath subject)
+		{
+			return subject.ToColumnName().ToUpperInvariant() + "_ID";
+		}
+	}
+}
