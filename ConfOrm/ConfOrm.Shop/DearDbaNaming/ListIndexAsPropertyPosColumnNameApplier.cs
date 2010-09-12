7b3ea172e42f59d45b40dd8c1c7570cc4a51e1d7
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-12 17:07:36 -0300

    Dear DBA ListIndexAsPropertyPosColumnNameApplier

---
+++ ConfOrm/ConfOrm.Shop/DearDbaNaming/ListIndexAsPropertyPosColumnNameApplier.cs
@@ -0,0 +1,23 @@
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+
+namespace ConfOrm.Shop.DearDbaNaming
+{
+	public class ListIndexAsPropertyPosColumnNameApplier: IPatternApplier<PropertyPath, IListPropertiesMapper>
+	{
+		public bool Match(PropertyPath subject)
+		{
+			return subject != null;
+		}
+
+		public void Apply(PropertyPath subject, IListPropertiesMapper applyTo)
+		{
+			applyTo.Index(lim=> lim.Column(GetIndexColumnName(subject)));
+		}
+
+		protected virtual string GetIndexColumnName(PropertyPath subject)
+		{
+			return subject.ToColumnName() + "_IDX";
+		}
+	}
+}
