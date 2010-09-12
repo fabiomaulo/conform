1cc057b32e7775cbcda51303c139ac4b44f75db5
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-12 17:26:59 -0300

    DearDbaTablesAndColumnsNamingPack (perhaps it need FK naming)

--- ConfOrm/ConfOrm.Shop/DearDbaNaming/DearDbaTablesAndColumnsNamingPack.cs
+++ ConfOrm/ConfOrm.Shop/DearDbaNaming/DearDbaTablesAndColumnsNamingPack.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using ConfOrm.Mappers;
 using ConfOrm.NH;
 using ConfOrm.Shop.Inflectors;
+using ConfOrm.Shop.NamingAppliers;
 
 namespace ConfOrm.Shop.DearDbaNaming
 {
@@ -21,12 +22,12 @@ namespace ConfOrm.Shop.DearDbaNaming
 			rootClass = new List<IPatternApplier<Type, IClassAttributesMapper>>
 			            	{
 			            		new ClassPluralizedTableApplier(inflector),
-											new PoidColumnNameApplier()
+			            		new PoidColumnNameApplier()
 			            	};
 			joinedSubclass = new List<IPatternApplier<Type, IJoinedSubclassAttributesMapper>>
 			                 	{
 			                 		new JoinedSubclassPluralizedTableApplier(inflector),
-													new JoinedSubclassKeyAsRootIdColumnApplier(domainInspector)
+			                 		new JoinedSubclassKeyAsRootIdColumnApplier(domainInspector)
 			                 	};
 			unionSubclass = new List<IPatternApplier<Type, IUnionSubclassAttributesMapper>>
 			                	{
@@ -34,7 +35,29 @@ namespace ConfOrm.Shop.DearDbaNaming
 			                	};
 			collectionPath = new List<IPatternApplier<PropertyPath, ICollectionPropertiesMapper>>
 			                 	{
-			                 		new ManyToManyPluralizedTableApplier(domainInspector, inflector)
+			                 		new ManyToManyPluralizedTableApplier(domainInspector, inflector),
+			                 		new ManyToManyKeyIdColumnApplier(domainInspector),
+			                 		new OneToManyKeyColumnApplier(domainInspector),
+			                 		new CollectionOfElementsTableApplier(domainInspector, inflector),
+			                 		new CollectionOfElementsKeyColumnApplier(domainInspector),
+			                 		new CollectionOfComponentsTableApplier(domainInspector, inflector),
+			                 		new CollectionOfComponentsKeyColumnApplier(domainInspector),
+			                 	};
+			propertyPath = new List<IPatternApplier<PropertyPath, IPropertyMapper>>
+			               	{
+			               		new ComponentPropertyColumnNameApplier(),
+			               	};
+			listPath = new List<IPatternApplier<PropertyPath, IListPropertiesMapper>>
+			           	{
+			           		new ListIndexAsPropertyPosColumnNameApplier(),
+			           	};
+			manyToOnePath = new List<IPatternApplier<PropertyPath, IManyToOneMapper>>
+			                	{
+			                		new ManyToOneColumnApplier()
+			                	};
+			manyToManyPath = new List<IPatternApplier<PropertyPath, IManyToManyMapper>>
+			                 	{
+			                 		new ManyToManyColumnApplier(domainInspector),
 			                 	};
 		}
 	}
