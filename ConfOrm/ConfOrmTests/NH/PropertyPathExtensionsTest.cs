c40ad6e724b6f869a229c0533e24c53e01229996
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-12 16:25:44 -0300

    Specific tests for PropertyPathExtensions

---
+++ ConfOrm/ConfOrmTests/NH/PropertyPathExtensionsTest.cs
@@ -0,0 +1,58 @@
+using ConfOrm;
+using ConfOrm.NH;
+using Moq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.NH
+{
+	public class PropertyPathExtensionsTest
+	{
+		private class MainEntity
+		{
+			public int Id { get; set; }
+		}
+		private class InheritedEntity
+		{
+			public MyComponent Component { get; set; }
+		}
+
+		private class MyComponent
+		{
+			public int Something { get; set; }
+			public MyComponent1 Component1 { get; set; }
+		}
+
+		private class MyComponent1
+		{
+			public int Something { get; set; }
+		}
+
+		[Test]
+		public void GetContainerEntityWhenPropertyIsInComponentThenReturnEntity()
+		{
+			var orm = new Mock<IDomainInspector>();
+			orm.Setup(x => x.IsEntity(typeof(MainEntity))).Returns(true);
+			orm.Setup(x => x.IsRootEntity(typeof(MainEntity))).Returns(true);
+			orm.Setup(x => x.IsEntity(typeof(InheritedEntity))).Returns(true);
+			var level0 = new PropertyPath(null, ForClass<InheritedEntity>.Property(p => p.Component));
+			var path = new PropertyPath(level0, ForClass<MyComponent>.Property(p => p.Something));
+
+			path.GetContainerEntity(orm.Object).Should().Be(typeof (InheritedEntity));
+		}
+
+		[Test]
+		public void GetContainerEntityWhenPropertyIsInDeppComponentThenReturnEntity()
+		{
+			var orm = new Mock<IDomainInspector>();
+			orm.Setup(x => x.IsEntity(typeof(MainEntity))).Returns(true);
+			orm.Setup(x => x.IsRootEntity(typeof(MainEntity))).Returns(true);
+			orm.Setup(x => x.IsEntity(typeof(InheritedEntity))).Returns(true);
+			var level0 = new PropertyPath(null, ForClass<InheritedEntity>.Property(p => p.Component));
+			var level1 = new PropertyPath(level0, ForClass<MyComponent>.Property(p => p.Component1));
+			var path = new PropertyPath(level1, ForClass<MyComponent1>.Property(p => p.Something));
+
+			path.GetContainerEntity(orm.Object).Should().Be(typeof(InheritedEntity));
+		}
+	}
+}
