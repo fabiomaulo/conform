04a8610d70069fa3fc33a33f2e709786e39087cd
Fabio Maulo <fabiomaulo@gmail.com>
2010-09-20 18:41:48 -0300

    Minor

--- ConfOrm/ConfOrm/Cascade.cs
+++ ConfOrm/ConfOrm/Cascade.cs
@@ -13,6 +13,20 @@ namespace ConfOrm
 		Detach= 32,
 		ReAttach= 64,
 		DeleteOrphans = 128,
-		All = 256
+		All = 256,
+
+		/// <summary>
+		/// It include each single <see cref="Cascade"/> value.
+		/// </summary>
+		/// <remarks>
+		/// It can be useful as a shortcut when a specific exclusion is needed as for example:
+		/// <example>
+		/// to exclude only the <see cref="Refresh"/>
+		/// <code>
+		/// ConfOrm.Cascade.Every & ~ConfOrm.Cascade.Refresh
+		/// </code>
+		/// </example>
+		/// </remarks>
+		Every = Persist | Refresh | Merge | Remove | Detach | ReAttach | DeleteOrphans
 	}
 }
