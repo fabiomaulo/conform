a69f82e6cf0dc232d89b8524aed05874c37105ed
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-01 19:32:51 -0300

    Fixed test and added test to check correct order of customization

--- ConfOrm/ConfOrmTests/NH/MapperTests/InterfacePropertiesPolymorphicCustomizationTest.cs
+++ ConfOrm/ConfOrmTests/NH/MapperTests/InterfacePropertiesPolymorphicCustomizationTest.cs
@@ -40,8 +40,28 @@ namespace ConfOrmTests.NH.MapperTests
 			HbmClass hbmMyClass1 = mappings.RootClasses.Single(hbm => hbm.Name == "MyClass1");
 			hbmMyClass1.Properties.OfType<HbmProperty>().Where(p=> p.Name == "Message").Single().length.Should().Be("123");
 
-			HbmClass hbmMyClass2 = mappings.RootClasses.Single(hbm => hbm.Name == "MyClass1");
+			HbmClass hbmMyClass2 = mappings.RootClasses.Single(hbm => hbm.Name == "MyClass2");
 			hbmMyClass2.Properties.OfType<HbmProperty>().Where(p => p.Name == "Message").Single().length.Should().Be("123");
+		}
+
+		[Test]
+		public void WhenCustomizePropertyOnInterfaceAndOverrideOnImplThenApplyCustomizationOnImplementations()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyClass1>();
+			orm.TablePerClass<MyClass2>();
+
+			var mapper = new Mapper(orm);
+			mapper.Customize<IHasMessage>(x => x.Property(hasMessage => hasMessage.Message, pm => pm.Length(123)));
+			mapper.Customize<MyClass2>(x => x.Property(hasMessage => hasMessage.Message, pm => pm.Length(456)));
+
+			var mappings = mapper.CompileMappingFor(new[] { typeof(MyClass1), typeof(MyClass2) });
+
+			HbmClass hbmMyClass1 = mappings.RootClasses.Single(hbm => hbm.Name == "MyClass1");
+			hbmMyClass1.Properties.OfType<HbmProperty>().Where(p => p.Name == "Message").Single().length.Should().Be("123");
+
+			HbmClass hbmMyClass2 = mappings.RootClasses.Single(hbm => hbm.Name == "MyClass2");
+			hbmMyClass2.Properties.OfType<HbmProperty>().Where(p => p.Name == "Message").Single().length.Should().Be("456");
 		}
 	}
 }
