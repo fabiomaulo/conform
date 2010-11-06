414d1a43b4f2a09fa69d6114ce23f3bee175eef6
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-06 12:31:28 -0300

    Moved class to core

--- ConfOrm/ConfOrm/PolymorphismResolver.cs
+++ ConfOrm/ConfOrm/PolymorphismResolver.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using ConfOrm;
 
-namespace ConfOrmTests.PolymorphismRelationsTests
+namespace ConfOrm
 {
 	public class PolymorphismResolver
 	{
--- ConfOrm/ConfOrmTests/PolymorphismRelationsTests/SimpleCaseWithoutComplexHierarchyTest.cs
+++ ConfOrm/ConfOrmTests/PolymorphismRelationsTests/SimpleCaseWithoutComplexHierarchyTest.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using ConfOrm;
 using NUnit.Framework;
 using SharpTestsEx;
 
