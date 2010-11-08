19ad8cbfad74b8c89248a26068adb9f10482044e
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-08 18:22:26 -0300

    Interface as relation : case-3 (component as element of collection with nested component)

--- ConfOrm/ConfOrmTests/InterfaceAsRelation/Case3CompositeElement.cs
+++ ConfOrm/ConfOrmTests/InterfaceAsRelation/Case3CompositeElement.cs
@@ -14,6 +14,7 @@ namespace ConfOrmTests.InterfaceAsRelation
 		{
 			public int Id { get; set; }
 			public IEnumerable<IHasMessage> Components { get; set; }
+			public IEnumerable<UpComponent> NestedComponents { get; set; }
 		}
 
 		private interface IHasMessage
@@ -24,6 +25,22 @@ namespace ConfOrmTests.InterfaceAsRelation
 		private class MyComponent : IHasMessage
 		{
 			public string Message { get; set; }
+		}
+
+		private interface IHasSomething
+		{
+			string Something { get; set; }
+		}
+
+		private class UpComponent
+		{
+			public int SomeValue { get; set; }
+			public IHasSomething NestedComponent { get; set; }
+		}
+
+		private class NestedComponent : IHasSomething
+		{
+			public string Something { get; set; }
 		}
 
 		[Test]
@@ -44,6 +61,26 @@ namespace ConfOrmTests.InterfaceAsRelation
 
 			hbmBagOfIRelation.ElementRelationship.Should().Be.OfType<HbmCompositeElement>()
 				.And.ValueOf.Class.Should().Contain("MyComponent");
+		}
+
+		[Test]
+		public void WhenInterfaceIsImplementedByNestedComponentThenRecognizeComponentWithTheCorrectClass()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+
+			orm.AddToDomain(typeof(UpComponent));
+			orm.AddToDomain(typeof(NestedComponent));
+
+			var mapper = new Mapper(orm);
+			var mapping = mapper.CompileMappingFor(new[] { typeof(MyEntity) });
+
+			HbmClass rc = mapping.RootClasses.First(r => r.Name.Contains("MyEntity"));
+			var hbmBagOfIRelation = (HbmBag)rc.Properties.Where(p => p.Name == "NestedComponents").Single();
+
+			var upComponent = (HbmCompositeElement)hbmBagOfIRelation.ElementRelationship;
+			upComponent.Properties.Where(p => p.Name == "NestedComponent").Single().Should().Be.OfType<HbmNestedCompositeElement>()
+				.And.ValueOf.Class.Should().Contain("NestedComponent");
 		}
 	}
 }
