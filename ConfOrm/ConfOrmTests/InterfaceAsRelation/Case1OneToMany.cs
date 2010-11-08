7327017ad8eaaa234ad4a2f9731e970931f048c0
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-08 14:22:38 -0300

    Interface as relation : case-1(one-to-many) not supported

---
+++ ConfOrm/ConfOrmTests/InterfaceAsRelation/Case1OneToMany.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Linq;
+using ConfOrm;
+using ConfOrm.NH;
+using NHibernate.Cfg.MappingSchema;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.InterfaceAsRelation
+{
+	public class Case1OneToMany
+	{
+		private class MyEntity
+		{
+			public int Id { get; set; }
+			public IEnumerable<IRelation> Relations { get; set; }
+			public IEnumerable<Relation1> Relations1 { get; set; }
+		}
+
+		private interface IRelation
+		{
+		}
+
+		private class MyRelation : IRelation
+		{
+			public int Id { get; set; }
+		}
+
+		private class Relation1
+		{
+		}
+
+		private class MyRelation1 : Relation1
+		{
+			public int Id { get; set; }
+		}
+
+		[Test, Ignore("Not supported yet.")]
+		public void WhenInterfaceIsImplementedByEntityThenRecognizeOneTManyWithTheCorrectClass()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+			orm.TablePerClass<MyRelation>();
+			orm.TablePerClass<MyRelation1>();
+
+			var mapper = new Mapper(orm);
+			var mapping = mapper.CompileMappingFor(new[] { typeof(MyEntity) });
+
+			HbmClass rc = mapping.RootClasses.First(r => r.Name.Contains("MyEntity"));
+			var hbmBagOfIRelation = (HbmBag) rc.Properties.Where(p => p.Name == "Relations").Single();
+			
+			hbmBagOfIRelation.ElementRelationship.Should().Be.OfType<HbmOneToMany>()
+				.And.ValueOf.Class.Should().Contain("MyRelation");
+		}
+
+		[Test, Ignore("Not supported yet.")]
+		public void WhenClassIsImplementedByEntityThenRecognizeOneToManyWithTheCorrectClass()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+			orm.TablePerClass<MyRelation>();
+			orm.TablePerClass<MyRelation1>();
+
+			var mapper = new Mapper(orm);
+			var mapping = mapper.CompileMappingFor(new[] { typeof(MyEntity) });
+
+			HbmClass rc = mapping.RootClasses.First(r => r.Name.Contains("MyEntity"));
+			var hbmBagOfIRelation = (HbmBag)rc.Properties.Where(p => p.Name == "Relations1").Single();
+
+			hbmBagOfIRelation.ElementRelationship.Should().Be.OfType<HbmOneToMany>()
+				.And.ValueOf.Class.Should().Contain("MyRelation1");
+		}
+	}
+}
