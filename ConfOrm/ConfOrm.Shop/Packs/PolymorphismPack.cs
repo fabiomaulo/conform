8f2cf5c4e56012e0d1e387d66b1b6af1ded19039
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-09 14:37:16 -0300

    Added PolymorphismPack for packs composition

---
+++ ConfOrm/ConfOrm.Shop/Packs/PolymorphismPack.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Patterns;
+
+namespace ConfOrm.Shop.Packs
+{
+	public class PolymorphismPack : EmptyPatternsAppliersHolder
+	{
+		public PolymorphismPack(IDomainInspector domainInspector)
+		{
+			manyToOne = new List<IPatternApplier<MemberInfo, IManyToOneMapper>>
+			            	{
+											new PolymorphismManyToOneClassApplier(domainInspector),
+			            	};
+			oneToMany = new List<IPatternApplier<MemberInfo, IOneToManyMapper>>
+									{
+										new PolymorphismOneToManyClassApplier(domainInspector)
+									};
+			component = new List<IPatternApplier<Type, IComponentAttributesMapper>>
+			            {
+			            	new PolymorphismComponentClassApplier(domainInspector),
+			            };
+
+		}
+	}
+}
