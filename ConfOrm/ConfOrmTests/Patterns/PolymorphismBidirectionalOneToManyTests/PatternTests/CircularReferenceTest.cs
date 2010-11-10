1bdb6c8310e56b4042d0eb1ff78d918155c8e27e
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-10 07:56:45 -0300

    Passing test for circular reference using interface

---
+++ ConfOrm/ConfOrmTests/Patterns/PolymorphismBidirectionalOneToManyTests/PatternTests/CircularReferenceTest.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using ConfOrm;
+using ConfOrm.Patterns;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.Patterns.PolymorphismBidirectionalOneToManyTests.PatternTests
+{
+	public class CircularReferenceTest
+	{
+		private interface INode
+		{
+			INode ParentNode { get; set; }
+			IEnumerable<INode> SubNodes { get; set; }
+		}
+
+		private class Node : INode
+		{
+			public int Id { get; set; }
+			public INode ParentNode { get; set; }
+			public IEnumerable<INode> SubNodes { get; set; }
+		}
+
+		[Test]
+		public void WhenInterfaceOnParentThenMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<Node>();
+
+			var pattern = new PolymorphismBidirectionalOneToManyMemberPattern(orm);
+			pattern.Match(ForClass<Node>.Property(x => x.SubNodes)).Should().Be.True();
+		}
+	}
+}
