7823a060d5b83c664ed317ddd5538a8dd6dc4c0b
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-10 20:13:49 -0300

    Added test

---
+++ ConfOrm/ConfOrmTests/Patterns/DelegatedAdvancedApplierTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Reflection;
+using ConfOrm.Mappers;
+using ConfOrm.Patterns;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.Patterns
+{
+	public class DelegatedAdvancedApplierTest
+	{
+		[Test]
+		public void WhenCreatedWithNullDelegatesThenThrow()
+		{
+			Executing.This(() => new DelegatedAdvancedApplier<MemberInfo, IPropertyMapper>(null, null)).Should().Throw<ArgumentNullException>();
+			Executing.This(() => new DelegatedAdvancedApplier<MemberInfo, IPropertyMapper>(x => true, null)).Should().Throw<ArgumentNullException>();
+			Executing.This(() => new DelegatedAdvancedApplier<MemberInfo, IPropertyMapper>(null, (x, y) => { })).Should().Throw<ArgumentNullException>();
+		}
+
+		[Test]
+		public void WhenCallMatchThenExecuteDelegate()
+		{
+			var executed = false;
+			var d = new DelegatedAdvancedApplier<MemberInfo, IPropertyMapper>(x => { executed = true; return true; }, (x, y) => { });
+			d.Match(null).Should().Be.True();
+			executed.Should().Be.True();
+		}
+
+		[Test]
+		public void WhenCallAppltThenExecuteDelegate()
+		{
+			var executed = false;
+			var d = new DelegatedAdvancedApplier<MemberInfo, IPropertyMapper>(x => true, (x, y) => executed = true);
+			d.Apply(null, null);
+			executed.Should().Be.True();
+		}
+	}
+}
