187799e6b5f8481122cbf589bb2a685a99e51460
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-14 13:50:06 -0300

    Bidirectional any-to-many pattern tests for case-6 root & no-root entities

---
+++ ConfOrm/ConfOrmTests/Patterns/PolymorphismBidirectionalOneToManyTests/AnyToMany/Case6MixedEntitiesPatternTests.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using ConfOrm;
+using ConfOrm.Patterns;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.Patterns.PolymorphismBidirectionalOneToManyTests.AnyToMany
+{
+	public class Case6MixedEntitiesPatternTests
+	{
+		private class MyEntity
+		{
+			public int Id { get; set; }
+			public IRelated Related { get; set; }
+		}
+
+		private interface IRelated
+		{
+
+		}
+
+		private class MyRoot1
+		{
+			public int Id { get; set; }
+		}
+
+		private class MyRelatedNoRoot1 : MyRoot1, IRelated
+		{
+			public IEnumerable<MyEntity> Items { get; set; }
+		}
+
+		private class MyRelatedRoot2 : IRelated
+		{
+			public int Id { get; set; }
+			public IEnumerable<MyEntity> Items { get; set; }
+		}
+
+		[Test]
+		public void WhenManySideIsNotAnEntityThenNoMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyRoot1>();
+			orm.TablePerClass<MyRelatedRoot2>();
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Match(ForClass<MyRelatedNoRoot1>.Property(x => x.Items)).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenTheDomainDoesNotContainMoreImplsThenNoMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+			orm.TablePerClass<MyRoot1>();
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Match(ForClass<MyRelatedNoRoot1>.Property(x => x.Items)).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenTheDomainContainsMoreImplsThenMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+			orm.TablePerClass<MyRoot1>();
+			orm.TablePerClass<MyRelatedRoot2>();
+
+			orm.AddToDomain(typeof(MyRelatedNoRoot1));
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Match(ForClass<MyRelatedNoRoot1>.Property(x => x.Items)).Should().Be.True();
+			pattern.Match(ForClass<MyRelatedRoot2>.Property(x => x.Items)).Should().Be.True();
+		}
+	}
+}
