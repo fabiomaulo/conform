67aba3095a6760e0e8c1fa71e79bde7ad490f3b9
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-14 13:22:32 -0300

    Bidirectional any-to-many pattern (tested for case-6)

---
+++ ConfOrm/ConfOrm/Patterns/PolymorphismBidirectionalAnyToManyPattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ConfOrm.Patterns
+{
+	public class PolymorphismBidirectionalAnyToManyPattern: IPattern<MemberInfo>
+	{
+		private readonly IDomainInspector domainInspector;
+
+		public PolymorphismBidirectionalAnyToManyPattern(IDomainInspector domainInspector)
+		{
+			if (domainInspector == null)
+			{
+				throw new ArgumentNullException("domainInspector");
+			}
+			this.domainInspector = domainInspector;
+		}
+
+		public bool Match(MemberInfo subject)
+		{
+			if (subject == null)
+			{
+				return false;
+			}
+			var declaredMany = subject.GetPropertyOrFieldType().DetermineCollectionElementType();
+			if (declaredMany == null)
+			{
+				return false;
+			}
+			if(!domainInspector.IsEntity(declaredMany))
+			{
+				return false;
+			}
+			var declaredOne = subject.ReflectedType;
+			List<Type> ancestorsOfOne = declaredOne.GetBaseTypes().ToList();
+			var cadidatedBidirectional = declaredMany.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy)
+				.Where(p => ancestorsOfOne.Contains(p.PropertyType)).FirstOrDefault(p=> domainInspector.IsHeterogeneousAssociation(p));
+			return cadidatedBidirectional != null;
+		}
+	}
+}
---
+++ ConfOrm/ConfOrmTests/Patterns/PolymorphismBidirectionalOneToManyTests/AnyToMany/Case6PatternTests.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using ConfOrm;
+using ConfOrm.Patterns;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ConfOrmTests.Patterns.PolymorphismBidirectionalOneToManyTests.AnyToMany
+{
+	public class Case6PatternTests
+	{
+		private class MyEntity
+		{
+			public int Id { get; set; }
+			public IRelatedRoot RelatedRoot { get; set; }
+		}
+
+		private interface IRelatedRoot
+		{
+
+		}
+
+		private class MyRelatedRoot1 : IRelatedRoot
+		{
+			public int Id { get; set; }
+			public IEnumerable<MyEntity> Items { get; set; }
+		}
+
+		private class MyRelatedRoot2 : IRelatedRoot
+		{
+			public int Id { get; set; }
+			public IEnumerable<MyEntity> Items { get; set; }
+		}
+
+		[Test]
+		public void CtorProtection()
+		{
+			Executing.This(() => new PolymorphismBidirectionalAnyToManyPattern(null)).Should().Throw<ArgumentNullException>();
+		}
+
+		[Test]
+		public void WhenSubjectIsNullThenNotThrow()
+		{
+			var orm = new ObjectRelationalMapper();
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Executing(p=> p.Match(null)).NotThrows();
+		}
+
+		[Test]
+		public void WhenManySideIsNotAnEntityThenNoMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyRelatedRoot1>();
+			orm.TablePerClass<MyRelatedRoot2>();
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Match(ForClass<MyRelatedRoot1>.Property(x => x.Items)).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenTheDomainDoesNotContainMoreImplsThenNoMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+			orm.TablePerClass<MyRelatedRoot1>();
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Match(ForClass<MyRelatedRoot1>.Property(x => x.Items)).Should().Be.False();
+		}
+
+		[Test]
+		public void WhenTheDomainContainsMoreImplsThenMatch()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.TablePerClass<MyEntity>();
+			orm.TablePerClass<MyRelatedRoot1>();
+			orm.TablePerClass<MyRelatedRoot2>();
+
+			var pattern = new PolymorphismBidirectionalAnyToManyPattern(orm);
+			pattern.Match(ForClass<MyRelatedRoot1>.Property(x => x.Items)).Should().Be.True();
+		}
+	}
+}
