17a79e7883d2a52cec363348576b9769a2d8a526
Fabio Maulo <fabiomaulo@gmail.com>
2010-11-27 16:09:43 -0300

    Added tests

--- ConfOrm/ConfOrmTests/EntitiesTypesTests.cs
+++ ConfOrm/ConfOrmTests/EntitiesTypesTests.cs
@@ -19,6 +19,34 @@ namespace ConfOrmTests
 			Contribute = 5,
 			Page = 7
 		}
+		public enum LongEnum : long
+		{
+			Something,
+		}
+		public enum ULongEnum : ulong
+		{
+			Something,
+		}
+		public enum IntEnum 
+		{
+			Something,
+		}
+		public enum UIntEnum : uint
+		{
+			Something,
+		}
+		public enum UShortEnum : ushort
+		{
+			Something,
+		}
+		public enum ByteEnum : byte
+		{
+			Something,
+		}
+		public enum SbyteEnum : sbyte
+		{
+			Something,
+		}
 
 		[Test]
 		public void WhenTypeOrNameIsNullThenThrows()
@@ -31,6 +59,30 @@ namespace ConfOrmTests
 		public void WhenTypeIsNotEnumThenThrows()
 		{
 			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(MyStruct), "Something")).Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		public void WhenNameFoundThenReturnValue()
+		{
+			EnumUtil.ParseGettingUnderlyingValue(typeof(ShortTypes), "Contribute").Should().Be((short)5);
+		}
+
+		[Test]
+		public void WhenNameNotFoundThenThrows()
+		{
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(ShortTypes), "Pizza")).Should().Throw<ArgumentException>();
+		}
+
+		[Test]
+		public void WorkWithAllTypesSupportedByEnum()
+		{
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(LongEnum), "Something")).Should().NotThrow();
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(ULongEnum), "Something")).Should().NotThrow();
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(IntEnum), "Something")).Should().NotThrow();
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(UIntEnum), "Something")).Should().NotThrow();
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(UShortEnum), "Something")).Should().NotThrow();
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(ByteEnum), "Something")).Should().NotThrow();
+			Executing.This(() => EnumUtil.ParseGettingUnderlyingValue(typeof(SbyteEnum), "Something")).Should().NotThrow();
 		}
 	}
 }
