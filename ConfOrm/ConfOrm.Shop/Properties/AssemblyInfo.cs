33459e5a844657e39388bdaacf4919b6d4db89b1
fabiomaulo <fabiomaulo@localhost>
2010-12-06 10:01:45 -0300

    Fixing version for release

--- ConfOrm/ConfOrm.Shop/Properties/AssemblyInfo.cs
+++ ConfOrm/ConfOrm.Shop/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Licensed under LGPL.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5ccd8759-c685-4840-ad28-7874cb12b22f")]
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
+[assembly: AssemblyVersion("1.0.1.2")]
+[assembly: AssemblyFileVersion("1.0.1.2")]
