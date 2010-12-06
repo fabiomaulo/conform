0bb108d2eba74e30315489c18cf712269de2b3df
fabiomaulo <fabiomaulo@localhost>
2010-12-06 09:32:22 -0300

    Fixed example

--- ConfOrm/ConfOrm.UsageExamples/StringProperty/Demo.cs
+++ ConfOrm/ConfOrm.UsageExamples/StringProperty/Demo.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using ConfOrm.NH;
 using ConfOrm.Shop.CoolNaming;
 using NHibernate;
@@ -10,25 +9,24 @@ namespace ConfOrm.UsageExamples.StringProperty
 	public class Demo
 	{
 		[Test, Explicit]
-		public void DefiningAndCustomizingVersionThroughBaseImplementation()
+		public void CustomizationOfStrings()
 		{
-			// In this example I'll show how you can work with Version and how ConfORM understands OOP
+			// In this example you can see how customize multiple properties in the same sentence.
+			// Specially have a look to how customize elements of a collection (in this cas a Bag).
 
 			var orm = new ObjectRelationalMapper();
 			var mapper = new Mapper(orm, new CoolPatternsAppliersHolder(orm));
-
-			// In this case I will use the definition of table-to-class strategy class by class
 			orm.TablePerClass<Customer>();
 
-            mapper.Customize<Customer>(c => c.Property(p => p.Name, pm =>
-                pm.Length(50)));
+			mapper.Class<Customer>(c =>
+			                           {
+																	 c.Property(p => p.Name, pm => pm.Length(50));
+																	 c.Property(p => p.Note, pm => pm.Type(NHibernateUtil.StringClob));
+																	 c.Bag(p => p.Notes, x => { }, map => map.Element(e=> e.Type(NHibernateUtil.StringClob)));
+			                           });
 
-            mapper.Customize<Customer>(c => c.Property(p => p.Notes, pm =>
-                pm.Type(NHibernateUtil.StringClob)));
-			
-			// Note : I have to create mappings for the whole domain; Entity and VersionedEntity are excluded from de mapping because out-side
-			// root-entities hierarchy (root-entities are : CurrencyDefinition, Company, Customer, Provider)
-			var mapping = mapper.CompileMappingFor(typeof(Entity).Assembly.GetTypes().Where(t => t.Namespace == typeof(Entity).Namespace));
+
+			var mapping = mapper.CompileMappingFor(new[] { typeof(Customer) });
 			Console.Write(mapping.AsString());
 		}
 	}
--- ConfOrm/ConfOrm.UsageExamples/StringProperty/Domain.cs
+++ ConfOrm/ConfOrm.UsageExamples/StringProperty/Domain.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ConfOrm.UsageExamples.StringProperty
 {
@@ -9,10 +10,13 @@ namespace ConfOrm.UsageExamples.StringProperty
 
 	public class Customer : Entity
 	{
-        //this is mapped as NVARCHAR(50)
+		//this is mapped as NVARCHAR(50)
 		public virtual string Name { get; set; }
 
-        //this is mapped as NVARCHAR(MAX)		
-        public virtual string Notes { get; set; }
+		//this is mapped as NVARCHAR(MAX)		
+		public virtual string Note { get; set; }
+
+		//Collection of NVARCHAR(MAX)		
+		public virtual IEnumerable<string> Notes { get; set; }
 	}
 }
