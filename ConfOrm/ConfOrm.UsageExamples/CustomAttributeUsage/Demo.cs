3c8cfc214ec6cbfe68268c443285a8d53e2227bb
Fabio Maulo <fabiomaulo@gmail.com>
2011-02-22 19:11:39 -0300

    Added example to avoid the implementation of a IPattern

--- ConfOrm/ConfOrm.UsageExamples/CustomAttributeUsage/Demo.cs
+++ ConfOrm/ConfOrm.UsageExamples/CustomAttributeUsage/Demo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using ConfOrm.NH;
 using NUnit.Framework;
 
@@ -11,6 +12,19 @@ namespace ConfOrm.UsageExamples.CustomAttributeUsage
 		{
 			var orm = new ObjectRelationalMapper();
 			orm.Patterns.Poids.Add(new PoidByAttributePattern());
+			orm.TablePerClass<CourseList>();
+
+			var mapper = new Mapper(orm);
+
+			var mapping = mapper.CompileMappingFor(new[] { typeof(CourseList) });
+			Console.Write(mapping.AsString());
+		}
+
+		[Test, Explicit]
+		public void UsageOfAttributeToSpecifyUsingDelegatesThePoID()
+		{
+			var orm = new ObjectRelationalMapper();
+			orm.Patterns.Poids.Add(mi => mi.GetCustomAttributes(typeof(IdAttribute), true).Any());
 			orm.TablePerClass<CourseList>();
 
 			var mapper = new Mapper(orm);
