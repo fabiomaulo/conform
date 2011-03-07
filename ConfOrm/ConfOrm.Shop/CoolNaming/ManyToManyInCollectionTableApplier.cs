b5bab31ae9dd02f7428efbfb91a8ef791dd1dbd5
Fabio Maulo <fabiomaulo@gmail.com>
2011-03-07 12:07:09 -0300

    Minor (cleaned)

--- ConfOrm/ConfOrm.Shop/CoolNaming/ManyToManyInCollectionTableApplier.cs
+++ ConfOrm/ConfOrm.Shop/CoolNaming/ManyToManyInCollectionTableApplier.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using ConfOrm.Mappers;
-using ConfOrm.NH;
-using ConfOrm.Shop.Appliers;
 using ConfOrm.Shop.NamingAppliers;
 
 namespace ConfOrm.Shop.CoolNaming
