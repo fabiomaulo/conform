6754bd10766f3d47fa0cdec0aea1573d8b7f498e
Fabio Maulo <fabiomaulo@gmail.com>
2011-03-16 12:17:30 -0300

    AccessToPropertyWhereNoFieldPack

---
+++ ConfOrm/ConfOrm.Shop/Packs/AccessToPropertyWhereNoFieldPack.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Reflection;
+using ConfOrm.Mappers;
+using ConfOrm.NH;
+using ConfOrm.Shop.Appliers;
+
+namespace ConfOrm.Shop.Packs
+{
+	public class AccessToPropertyWhereNoFieldPack : EmptyPatternsAppliersHolder
+	{
+		public AccessToPropertyWhereNoFieldPack()
+		{
+			poid = new List<IPatternApplier<MemberInfo, IIdMapper>>
+			       	{
+			       		new AccessToPropertyWhereNoFieldApplier<IIdMapper>(),
+			       	};
+
+			property = new List<IPatternApplier<MemberInfo, IPropertyMapper>>
+			           	{
+			           		new AccessToPropertyWhereNoFieldApplier<IPropertyMapper>(),
+			           	};
+
+			collection = new List<IPatternApplier<MemberInfo, ICollectionPropertiesMapper>>
+			             	{
+			             		new AccessToPropertyWhereNoFieldApplier<ICollectionPropertiesMapper>(),
+			             	};
+
+			manyToOne = new List<IPatternApplier<MemberInfo, IManyToOneMapper>>
+			            	{
+			            		new AccessToPropertyWhereNoFieldApplier<IManyToOneMapper>(),
+			            	};
+
+			oneToOne = new List<IPatternApplier<MemberInfo, IOneToOneMapper>>
+			           	{
+			           		new AccessToPropertyWhereNoFieldApplier<IOneToOneMapper>(),
+			           	};
+
+			any = new List<IPatternApplier<MemberInfo, IAnyMapper>>
+			      	{
+			      		new AccessToPropertyWhereNoFieldApplier<IAnyMapper>(),
+			      	};
+			componentParent = new List<IPatternApplier<MemberInfo, IComponentParentMapper>>
+			                  	{
+			                  		new AccessToPropertyWhereNoFieldApplier<IComponentParentMapper>(),
+			                  	};
+			componentProperty = new List<IPatternApplier<MemberInfo, IComponentAttributesMapper>>
+			                    	{
+			                    		new AccessToPropertyWhereNoFieldApplier<IComponentAttributesMapper>(),
+			                    	};
+		}
+	}
+}
