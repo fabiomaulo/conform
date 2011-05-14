2f18ad0e77fed4a1c3369ae9dff80688ba82d7d5
Fabio Maulo <fabiomaulo@gmail.com>
2011-05-14 13:39:36 -0300

    Minor

--- ConfOrm/NuGetPush.bat
+++ ConfOrm/NuGetPush.bat
@@ -1,3 +1,3 @@
 rem The parameter is your NuGet access key 
 nuget pack ConfORM.nuspec
-nuget push -source http://packages.nuget.org/v1/ ConfOrm.1.0.1.4.nupkg %1 
+nuget push -source http://packages.nuget.org/v1/ ConfOrm.1.0.1.5.nupkg %1 
