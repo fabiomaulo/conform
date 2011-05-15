57bc409bbb309257d1d196d27a80bf382393c737
Fabio Maulo <fabiomaulo@gmail.com>
2011-05-15 16:59:39 -0300

    changed version to actual development

--- ConfOrm/ConfOrm.Shop/Properties/AssemblyInfo.cs
+++ ConfOrm/ConfOrm.Shop/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Licensed under LGPL.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5ccd8759-c685-4840-ad28-7874cb12b22f")]
-[assembly: AssemblyVersion("1.0.1.5")]
-[assembly: AssemblyFileVersion("1.0.1.5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
--- ConfOrm/ConfOrm/Properties/AssemblyInfo.cs
+++ ConfOrm/ConfOrm/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Licensed under LGPL.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e8259277-bf60-47c1-94f7-1e02626f55a2")]
-[assembly: AssemblyVersion("1.0.1.5")]
-[assembly: AssemblyFileVersion("1.0.1.5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
